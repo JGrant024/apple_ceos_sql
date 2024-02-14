2a4a6c747ada94f5ce17e27f694c6e79be853ab0
JGrant024 <jonathan.grant23@icloud.com>
2024-02-13 23:12:46 -0500

    updated apple ceo exercise

---
+++ schema.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE apple_ceo (
+    id serial PRIMARY KEY
+    ceo_name text 
+    slug text
+    first_year_active integer
+)
+ 
+ 
---
+++ seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO apple_ceos (Name, Slug, First_Year_Active) VALUES 
+('Michael Spindler', 'michael_spindler', 1993),
+('Gil Amelio', 'gil_amelio', 1996),
+('Mike Markkula', 'mike_markkula', 1977),
+('Tim Cook', 'tim_cook', 2011),
+('Mike Scott', 'mike_scott', 1978),
+('John Sculley', 'john_sculley', 1983),
+('Steve Jobs', 'steve_jobs_1976', 1976),
+('Steve Jobs', 'steve_jobs_1997', 1997);
